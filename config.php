daeb19b81447b24c19249b870993f2dd74bc0ae8
EkonaldiHutabarat <ekonaldihutabarat@gmail.com>
2021-04-29 23:43:52 +0700

    tooolooooooooooooong

--- config.php
+++ config.php
@@ -15,7 +15,7 @@ $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => '',
   'dbsocket' => '',
-  'dbcollation' => 'utf8_unicode_ci',
+  'dbcollation' => 'utf8_general_ci',
 );
 //https://localhost/AllProject/TA1/moodle
 // /opt/lampp/moodledata
